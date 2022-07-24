85c8510301754956c712fc13b18c77d4ee3e07e6
Lucy <lucy70381@kimo.com>
2022-07-25 00:46:13 +0800

    week3~4 - 眼睛形象RWD網頁

---
+++ Week3/app/assets/js/all.js
@@ -0,0 +1,5 @@
+$(".menuBtn").click(()=> {
+    $(".menuBtn").css("display", "none");
+    $(".header-container").css("justify-content", "center");
+    $(".menu-container").css("display", "block")
+})
---
+++ Week3/dist/assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(".menuBtn").click(function () {
+  $(".menuBtn").css("display", "none");
+  $(".header-container").css("justify-content", "center");
+  $(".menu-container").css("display", "block");
+});
+//# sourceMappingURL=all.js.map
